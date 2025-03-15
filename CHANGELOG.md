ecd7b550a3ce96206c9d689217a9c95f62d471ba
Paul E. Jones <paulej@packetizer.com>
2025-03-15 16:33:16 -0400

    Changes to build on OpenBSD

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+v1.0.3
+
+- Changes required to build on OpenBSD
+
 v1.0.2
 
 - Updated to the latest Simple Test Framework (STF)
--- src/logger.cpp
+++ src/logger.cpp
@@ -20,7 +20,7 @@
 #include <chrono>
 #include <ctime>
 #include <iomanip>
-#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__)
 #include <syslog.h>
 #define SYSLOG_SUPPORTED 1
 #else
